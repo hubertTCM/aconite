0ddc8f395a79a85892abdf98d3ac073cc52af0f7
Hubert Fu <fuqunxing@gmail.com>
2021-12-06 23:05:59 +1300

    enable i18n

--- spa/src/index.tsx
+++ spa/src/index.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { enUS } from "@mui/material/locale";
+
 import { App } from "./app";
 
+// l10n: https://mui.com/guides/localization/
+const theme = createTheme(enUS);
+
 try {
-  render(<App />, document.getElementById("app"));
+  render(
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>,
+    document.getElementById("app")
+  );
 } catch (err) {
   console.error(err); // TODO: redirect to global error page
 }
